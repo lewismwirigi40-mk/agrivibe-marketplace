8a0637cfb91e3f1cf418ac51e4fc63950b915981
Lewis Mwirigi <lewismwirigi40@gmail.com>
2026-08-20 09:56:21 +0300

    Restore true marketplace interface with absolute centered alignment parameters

--- AgriVibe/src/pages/index.tsx
+++ AgriVibe/src/pages/index.tsx
@@ -161,7 +161,8 @@ export default function Home() {
       <FloatingButtons />
 
       {/* ====== HERO SECTION ====== */}
-      <section className="relative overflow-hidden px-4 pt-32 pb-20 md:pt-40 md:pb-28 min-h-[90vh] flex items-center">
+      <section className="relative overflow-hidden px-4 pt-32 pb-20 md:pt-40 md:pb-28 min-h-[90vh] flex flex-col items-center justify-center text-center w-full">
+
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat scale-110"
           style={{
